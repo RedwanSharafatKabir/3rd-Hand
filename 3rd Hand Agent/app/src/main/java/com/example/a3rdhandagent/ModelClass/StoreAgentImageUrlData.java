66d20b6907e76caff66a149ea346700e3c716860
Redwan Sharafat Kabir <mohammadmunna478@gmail.com>
2021-04-01 11:40:04 +0600

    Social service app

---
+++ 3rd Hand Agent/app/src/main/java/com/example/a3rdhandagent/ModelClass/StoreAgentImageUrlData.java
@@ -0,0 +1,19 @@
+package com.example.a3rdhandagent.ModelClass;
+
+public class StoreAgentImageUrlData {
+    String avatar;
+
+    public StoreAgentImageUrlData() {}
+
+    public StoreAgentImageUrlData(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+}
